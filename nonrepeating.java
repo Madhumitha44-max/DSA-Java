df62ce1ed0cd6869b3e2880082ba2eebafd78853
Madhumitha44-max <madhumithadommati@gmail.com>
2026-06-09 23:55:28 +0530

    Day 9 of DSA journey

--- nonrepeating.java
+++ nonrepeating.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
-public class sorting{
+public class nonrepeating{
     public static void main(String[] args){
-Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array");
         int n=sc.nextInt();
        int[] num=new int[n];
+       System.out.println("Enter the elements of the array: ");
        for(int i=0;i<num.length;i++){
         num[i]=sc.nextInt();
        }
-       for(int i=0;i<num.length;i++){
+for(int i=0;i<num.length;i++){
               for(int j=0;j<num.length-1;j++){
 if(num[j]>num[j+1]){
 int temp=num[j+1];
@@ -16,10 +18,17 @@ num[j]=temp;
 }
               }
        }
-       System.out.print("sorted array is  ");
+       int count=0;
+for(int i=0;i<num.length-1;i++){
+if(num[i]==num[i+1]){
+count++;
+}
+}
        for(int i=0;i<num.length;i++){
-System.out.println(num[i] +" ");
+if(count==1){
+System.out.println("The non-repeating number is : "+num[i]);
+}
        }
 
-    }
+}
 }
