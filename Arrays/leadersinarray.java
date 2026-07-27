a33199f6763415a57af38488c71c18d74f5e46d1
Madhumitha44-max <madhumithadommati@gmail.com>
2026-07-27 22:43:17 +0530

    Implement leaders in an array problem

--- Arrays/leadersinarray.java
+++ Arrays/leadersinarray.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class leadersinarray {
 
      public static void findLeader(int[] num) {
-      int max=Integer.MIN_VALUE;
-      ArrayList<Integer> temp=new ArrayList<>();
-      for(int i=num.length-1;i>=0;i--) {
-        if(num[i]>=max) {
+      int max = Integer.MIN_VALUE;
+      ArrayList<Integer> temp = new ArrayList<>();
+      for(int i = num.length-1; i >= 0; i--) {
+
+        if(num[i] >=  max) {
             temp.add(num[i]);
-            max=num[i];
+            max = num[i];
         }
+
       }
       System.out.println(temp);
      }
